016ecc9f10124a2c97dbfcab025488b497e21456
dvalerio <dnotms290>
2019-10-29 16:27:24 -0300

    Imagens usadas no projeto

---
+++ lib/PaginaPrincipal.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class PaginaPrincipal extends StatefulWidget {
+  @override
+  _PaginaPrincipalState createState() => _PaginaPrincipalState();
+}
+
+class _PaginaPrincipalState extends State<PaginaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: <Widget>[
+          //imagem
+
+          Padding(
+            padding: EdgeInsets.only(top:20),
+            child: Row(
+
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
